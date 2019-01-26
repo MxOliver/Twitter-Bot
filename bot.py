5aad9868b8a283ea3b1279f0f86a4d745543cea6
Oliver Coley <mxoliverleigh@gmail.com>
2019-01-26 17:19:28 -0500

    environ

--- bot.py
+++ bot.py
@@ -4,6 +4,12 @@ import tweepy
 from time import sleep 
 from credentials import *
 
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
