23383c38e72d41a6d4aa3413dffcddd2061732fd
Oliver Coley <mxoliverleigh@gmail.com>
2019-01-28 18:26:16 -0500

    update api

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 words = open('source.txt').read()
